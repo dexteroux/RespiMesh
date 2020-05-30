11a7ed40dbe521bbad77508a8408be23816513f7
dexteroux <dexteroux@gmail.com>
2020-05-30 10:42:45 +0000

    wpasupplicant removed

--- initsetup.sh
+++ initsetup.sh
@@ -1,3 +1,4 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install batctl iw alfred tlp
+sudo apt remove wpasupplicant
--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo cp 100-mesh-init.yaml /etc/netplan/100-mesh-init.yaml
-sudo netplan --debug apply
-# Ensure the batman-adv kernel module is loaded at boot time
-echo 'batman-adv' | sudo tee --append /etc/modules
-# Stop the DHCP process from trying to manage the wireless lan interface
-echo 'denyinterfaces wlan0' | sudo tee --append /etc/dhcpcd.conf
+#sudo cp 100-mesh-init.yaml /etc/netplan/100-mesh-init.yaml
+#sudo netplan --debug apply
+## Ensure the batman-adv kernel module is loaded at boot time
+#echo 'batman-adv' | sudo tee --append /etc/modules
+## Stop the DHCP process from trying to manage the wireless lan interface
+#echo 'denyinterfaces wlan0' | sudo tee --append /etc/dhcpcd.conf
 
 sudo mkdir -p /etc/scripts
 sudo cp start-batman-adv.sh /etc/scripts/start-batman-adv.sh
